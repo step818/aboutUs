6d5773775712901c54605a73790cc5bb4c2354f0
Stephen Trewick <stephent818@gmail.com>
2019-07-01 13:27:53 -0700

    added an image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import carlsagan from '../assets/images/carlsagan.jpg';
 
 function About() {
   return(
     <div>
-      <p>some stuff</p>
+      <h3>These people work here</h3>
+      <img src={carlsagan}/>
+      <h3>Carl Sagan</h3>
+      <p>The man, the legend, the myth.</p>
     </div>
   );
 }
